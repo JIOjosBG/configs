1293029085a37f67641d7704bce251f31b54ef1d
Yosif Hameed <josif.hameed@gmail.com>
2024-03-09 02:54:55 +0200

    iAdd basic nvim shortcuts

--- nvim/lua/custom-keys.lua
+++ nvim/lua/custom-keys.lua
@@ -2,5 +2,19 @@
 local map = vim.api.nvim_set_keymap
 
 -- map the key n to run the command :NvimTreeToggle
-map('n', 'n', [[:NvimTreeToggle<CR>]], {})
-map('i', '<C-n>', '<cmd>NvimTreeToggle<CR>', {})
+map('n', '<C-e>', [[:NvimTreeToggle<CR>]], {})
+map('i', '<C-e>', '<cmd>NvimTreeToggle<CR>', {})
+-- basic commands
+map('i', '<C-s>', '<cmd>:w<CR>', {noremap = true, silent = true})
+
+-- these are replaced for Ctrl+W +q, because C-w already has window tiling functionality
+-- map('i', '<C-w>', '<cmd>:q<CR>', {noremap = true, silent = true})
+-- map('n', '<C-w>', [[:q]], {noremap = true, silent = true})
+
+map('i', '<C-x>', '<cmd>:y | :d<CR>', {noremap = true, silent = true})
+
+
+map('i', '<C-c>', '<cmd>:y<CR>', {noremap = true, silent = true})
+
+
+map('i', '<C-v>', '<cmd>:pu<CR>', {noremap = true, silent = true})
